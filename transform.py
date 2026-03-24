0453bb5c067e7dd9f9f1e5f49a16a73b7ecf1b85
William Kim <willkim0117@gmail.com>
2026-03-24 22:59:36 +1100

    Updated

--- transform.py
+++ transform.py
@@ -25,3 +25,26 @@ def calc_beat_streak(df):
         else:
             break         
     return streak
+
+
+def calc_price_reaction(eps_df, price_df, days_after=1):
+    reactions = []
+
+    for _, row in eps_df.iterrows():
+        date = pd.to_datetime(row["date"])
+
+        if date not in price_df.index:
+            continue 
+
+        future_dates = price_df.index[price_df.index > date]
+        if len(future_dates) < days_after:
+            continue
+
+        next_day = future_dates[days_after - 1]
+        price_before = price_df.loc[date, "close"]
+        price_after  = price_df.loc[next_day, "close"]
+
+        reaction_pct = ((price_after - price_before) / price_before * 100).round(2)
+        reactions.append({ "date": date, "reaction_pct": reaction_pct })
+
+    return pd.DataFrame(reactions)
